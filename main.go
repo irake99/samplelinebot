7b2a03ee511ff615842138794d39968680ad113f
Jack (Lung-Hsuan) Hung <lhhungx@gmail.com>
2023-05-17 02:46:54 +0800

    Add a model package using MongoDB
    To save and query user messages.

--- main.go
+++ main.go
@@ -4,13 +4,70 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"reflect"
+	"time"
+
+	"samplelinebot/pkg/model"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 type config struct {
-	ServerPort string `mapstructure:"SERVER_PORT"`
+	ServerPort           string `mapstructure:"SERVER_PORT"`
+	MongoURI             string `mapstructure:"MONGO_URI"`
+	MongoUsername        string `mapstructure:"MONGO_INITDB_ROOT_USERNAME"`
+	MongoPassword        string `mapstructure:"MONGO_INITDB_ROOT_PASSWORD"`
+	LineBotChannelSecret string `mapstructure:"LINEBOT_CHANNEL_SECRET"`
+	LineBotChannelToken  string `mapstructure:"LINEBOT_CHANNEL_TOKEN"`
+}
+
+// Check is any config value missing
+func (c config) MissingValue() []string {
+	missings := []string{}
+	val := reflect.ValueOf(c)
+	for i := 0; i < val.NumField(); i++ {
+		switch v := val.Field(i).Interface().(type) {
+		case string:
+			if v == "" {
+				missings = append(missings, val.Type().Field(i).Name)
+			}
+		default:
+			fmt.Printf("unexpected type of config value, field name: %s, type: %T\n", val.Type().Field(i).Name, v)
+		}
+	}
+
+	return missings
+}
+
+func tryModel(cfg *config) {
+	model.InitClient(cfg.MongoURI, cfg.MongoUsername, cfg.MongoPassword)
+	defer func() {
+		if err := model.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	err := model.TestConnection()
+	if err != nil {
+		fmt.Printf("failed test of MongoDB connection: %s", err)
+	}
+
+	userID := "U1bacf29aaf2e34111fb"
+	msg := model.UserMessage{
+		UserName:    "Test User 1",
+		UserID:      userID,
+		MessageText: "Hello",
+		Timestamp:   time.Now(),
+	}
+
+	msg.Save()
+	history, err := model.GetHistory(userID)
+	if err != nil {
+		panic(fmt.Sprintf("failed of getting message history: %v", err))
+	}
+	historyText := model.FormatHistory(history)
+	fmt.Println(historyText)
 }
 
 var rootCmd = &cobra.Command{
@@ -21,8 +78,13 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-func loadConfig() *config {
+func loadConfig() (*config, error) {
 	viper.BindEnv("SERVER_PORT")
+	viper.BindEnv("MONGO_URI")
+	viper.BindEnv("MONGO_INITDB_ROOT_USERNAME")
+	viper.BindEnv("MONGO_INITDB_ROOT_PASSWORD")
+	viper.BindEnv("LINEBOT_CHANNEL_SECRET")
+	viper.BindEnv("LINEBOT_CHANNEL_TOKEN")
 
 	cfg := &config{}
 	err := viper.Unmarshal(cfg)
@@ -30,12 +92,21 @@ func loadConfig() *config {
 		log.Fatalln(err)
 	}
 
-	return cfg
+	if missings := cfg.MissingValue(); len(missings) != 0 {
+		return nil, fmt.Errorf("missing values from the config: %+v", missings)
+	}
+
+	return cfg, nil
 }
 
 func main() {
-	cfg := loadConfig()
+	cfg, err := loadConfig()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Printf("The server port is %s\n", cfg.ServerPort)
+
+	tryModel(cfg)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
---
+++ pkg/model/model.go
@@ -0,0 +1,102 @@
+package model
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var (
+	client *mongo.Client
+	ctx    context.Context
+	cancel context.CancelFunc
+)
+
+var timeout = 10 * time.Second
+
+type UserMessage struct {
+	UserName    string    `bson:"user_name,omitempty"`
+	UserID      string    `bson:"user_id,omitempty"`
+	MessageText string    `bson:"message_text,omitempty"`
+	Timestamp   time.Time `bson:"timestamp,omitempty"`
+}
+
+func (msg *UserMessage) Save() {
+	coll := client.Database("linebot").Collection("messages")
+
+	_, err := coll.InsertOne(context.Background(), msg)
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func GetHistory(userID string) ([]UserMessage, error) {
+	filter := bson.D{primitive.E{Key: "user_id", Value: userID}}
+
+	coll := client.Database("linebot").Collection("messages")
+	cursor, err := coll.Find(context.Background(), filter)
+	if err != nil {
+		return nil, fmt.Errorf("failed of query messages: %w", err)
+	}
+
+	var msgs []UserMessage
+	if err = cursor.All(context.Background(), &msgs); err != nil {
+		return nil, fmt.Errorf("failed of fetching and decoding messages: %w", err)
+	}
+
+	return msgs, nil
+}
+
+func FormatHistory(msgs []UserMessage) string {
+	tz, _ := time.LoadLocation("Local")
+	var b strings.Builder
+	b.Grow(200)
+	b.WriteString("Your history:\n")
+	for _, msg := range msgs {
+		fmt.Fprintf(&b, "* %s | %s\n", msg.Timestamp.In(tz).Format("2006/01/02 15:04:05 MST"), msg.MessageText)
+	}
+
+	return b.String()
+}
+
+func InitClient(uri, username, password string) error {
+	var err error
+
+	credential := options.Credential{
+		Username: username,
+		Password: password,
+	}
+
+	clientOpts := options.Client().ApplyURI(uri).SetAuth(credential)
+
+	ctx, cancel = context.WithTimeout(context.Background(), timeout)
+
+	client, err = mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		return fmt.Errorf("failed of connect the MongoDB: %w", err)
+	}
+
+	return nil
+}
+
+func Disconnect() error {
+	defer cancel()
+
+	if err := client.Disconnect(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestConnection() error {
+	return client.Ping(ctx, readpref.Primary())
+}
